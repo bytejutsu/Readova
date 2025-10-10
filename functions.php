f8065a8fb7fc72ee61ce721ad78452877a31d20e
Dhia Nabli <bytejutsuproject@gmail.com>
2025-10-10 21:01:07 +0100

    enqueued the css styles and scripts properly

--- functions.php
+++ functions.php
@@ -1,2 +1,40 @@
 <?php
 
+function readova_scripts() {
+    // --- Styles ---
+    wp_enqueue_style('readova-fonts','https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;600;700&display=swap', array(), null);
+    wp_enqueue_style('readova-bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), null);
+    wp_enqueue_style('readova-bootstrap-icons-css', get_template_directory_uri() . '/css/bootstrap-icons.css', array(), null);
+    wp_enqueue_style('readova-main-css', get_template_directory_uri() . '/css/main.css');
+
+    // --- Dequeue WordPress' built-in jQuery ---
+    wp_deregister_script('jquery');
+
+    wp_register_script(
+        'jquery',
+        get_template_directory_uri() . '/js/jquery-2.2.3.min.js',
+        array(),
+        '2.2.3',
+        true
+    );
+
+    // --- Enqueue older jquery version ---
+    wp_enqueue_script('jquery');
+
+    // --- Other scripts ---
+    wp_enqueue_script('readova-bootstrap-js', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array('jquery2'), null, true);
+    wp_enqueue_script('readova-sticky-js', get_template_directory_uri() . '/js/jquery.sticky.js', array('jquery2'), null, true);
+    wp_enqueue_script('readova-click-scroll-js', get_template_directory_uri() . '/js/click-scroll.js', array(), null, true);
+    wp_enqueue_script('readova-custom-js', get_template_directory_uri() . '/js/custom.js', array(), null, true);
+
+}
+
+function readova_preconnect() {
+    echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
+    echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+}
+
+add_action('wp_enqueue_scripts', 'readova_scripts');
+
+add_action('wp_head', 'readova_preconnect', 1);
+
--- index.php
+++ index.php
@@ -3,36 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <meta name="description" content="">
         <meta name="author" content="">
-
-        <title>ebook landing page template</title>
-
-        <!-- CSS FILES -->        
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-        <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;600;700&display=swap" rel="stylesheet">
-
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
-
-        <link href="css/templatemo-ebook-landing.css" rel="stylesheet">
-<!--
-
--->
+        <?php wp_head(); ?>
     </head>
     
     <body>
-
         <main>
-
             <nav class="navbar navbar-expand-lg">
                 <div class="container">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="#">
                         <i class="navbar-brand-icon bi-book me-2"></i>
                         <span>ebook</span>
                     </a>
